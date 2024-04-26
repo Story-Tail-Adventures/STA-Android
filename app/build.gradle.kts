57ac20f6a9b0d4812368c44453f46cff70ebf26f
Gyasi Story <gyasistory@gmail.com>
2024-04-26 12:17:49 -0400

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation(libs.kotlin.kover.gradlePlugin)
 }
