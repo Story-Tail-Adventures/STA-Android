e0550cda9418aacd71dc912a1b7f2fedae6ded21
Gyasi Story <gyasistory@gmail.com>
2024-06-03 16:42:47 -0400

    Add Specific Color

--- composeApp/src/commonMain/kotlin/theme/StoryTailColorScheme.kt
+++ composeApp/src/commonMain/kotlin/theme/StoryTailColorScheme.kt
@@ -4,12 +4,12 @@ import androidx.compose.material.Colors
 import androidx.compose.ui.graphics.Color
 
 val StoryTailColors = Colors(
-    primary= Color.Red,
+    primary= Color(red = 120, green = 0, blue = 0),
     primaryVariant= Color.Magenta,
-    secondary= Color.Yellow,
-    secondaryVariant= Color.Unspecified,
-    background= Color.Unspecified,
-    surface= Color.Unspecified,
+    secondary= Color(red = 232, green = 140, blue = 27),
+    secondaryVariant= Color.Yellow,
+    background= Color.White,
+    surface= Color.White,
     error= Color.Unspecified,
     onPrimary= Color.White,
     onSecondary= Color.DarkGray,
@@ -18,3 +18,4 @@ val StoryTailColors = Colors(
     onError= Color.Unspecified,
     isLight= true
 )
+
